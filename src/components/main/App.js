ae85a0de5c325374bf737ad931a9919669d44cb0
MoiOu <hak92995@educastur.es>
2025-10-22 13:20:14 +0200

    Actividad 1 UT3 Subida

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -4,19 +4,20 @@ import Header from '../header/Header';
 import Footer from '../footer/Footer';
 
 function App() {
+  
   return (
-    <div className="App">
+    <>
         <Header/>
-        <h1>Hola Mundo </h1>
-        <h2>Este es mi primer composable React</h2>
+        <h1>Mi aplicación</h1>
+        
         <div className="Parrafo">
-          <p>Bienvenido a mi aplicación, esto fue creado con JavaScript en React</p>
+          <p>Este es el contenido de la app: </p>
+
+          <MiLista></MiLista>
         </div>
-        <MiLista titulo=" Persona1" nombre1="Paco" nombre2="Lucas" nombre3="Ainara" nombre4="Rubén"/>
-        <MiLista titulo=" Persona2" nombre1="Paco" nombre2="Lucas" nombre3="Ainara" nombre4="Rubén"/>
-        <MiLista titulo=" Persona3" nombre1="Paco" nombre2="Lucas" nombre3="Ainara" nombre4="Rubén"/>
+        
        <Footer/> 
-    </div>
+    </>
   );
 }
 
